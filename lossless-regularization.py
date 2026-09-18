509385c390fc5d0f9cd6854aef81a9630bfcaa99
Emmanuel I. Obi <withtwoemms@gmail.com>
2026-09-17 20:56:13 -0500

    fixes the sampling window to match the kernel, and modernizes dependency pins

--- lossless-regularization.py
+++ lossless-regularization.py
@@ -24,17 +24,17 @@ def gen_tolerance_range(period, multiplier=1, spread=1):
     tolerance_range = range(multiplier * period - spread, multiplier * period + spread + 1)
     if len(tolerance_range) % 2 == 0:
         raise ValueError("Tolerance range must have an odd number.")
-    print(multiplier, tolerance_range)
     return tolerance_range
 
 
 @click.command()
-@click.option('--count', default=20, type=int, help='Number of greetings.')
+@click.option('--count', default=20, type=int, help='Number of timestamps to generate.')
 @click.option('--period', prompt='Mean period', type=int, help='Nominal period of sensor.')
 def cli(period, count):
-    ndgk = normalize(discrete_gaussian_kernel(len(gen_tolerance_range(period, multiplier=2, spread=3))))
+    spread = 3
+    ndgk = normalize(discrete_gaussian_kernel(len(gen_tolerance_range(period, spread=spread))))
     timestamps = [
-        sample_from_distribution(ndgk, gen_tolerance_range(i, period))[-1]
+        int(sample_from_distribution(ndgk, gen_tolerance_range(period, multiplier=i, spread=spread))[-1])
         for i in range(count)
         if i > 0
     ]
